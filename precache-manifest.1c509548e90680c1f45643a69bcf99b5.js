a652baae8fd4a3c6a7576427ccb09039b1c2a41a
Joe Hanson <joe@pubnub.com>
2019-04-17 11:24:34 -0700

    commit

---
+++ precache-manifest.1c509548e90680c1f45643a69bcf99b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  },
+  {
+    "revision": "6f1c16a43035a24143db",
+    "url": "/WeatherApp/static/js/main.1fdf65c4.chunk.js"
+  },
+  {
+    "revision": "1a0a3bfa4926b055ef74",
+    "url": "/WeatherApp/static/js/2.a22f1cdf.chunk.js"
+  },
+  {
+    "revision": "6f1c16a43035a24143db",
+    "url": "/WeatherApp/static/css/main.9e2dd241.chunk.css"
+  },
+  {
+    "revision": "a362dad8e17acf6f57e045cf7f9a308d",
+    "url": "/WeatherApp/index.html"
+  }
+];
